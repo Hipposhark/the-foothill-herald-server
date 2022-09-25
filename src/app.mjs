258fd0fc3a9b9b736a20a065fcf1b23cf1496bd7
Hipposhark <edwin.li@outlook.com>
2022-09-25 01:41:07 -0700

    updated cors

--- src/app.mjs
+++ src/app.mjs
@@ -3,14 +3,7 @@ import cors from "cors";
 
 export const app = ({userRoutes, articleRoutes, servingMiddleware, errorMiddleware}) => {
     const app = express()
-    app.use(cors({
-        credentials: true,
-        origin: [
-            'http://localhost:3000', 
-            'http://the-foothill-herald.herokuapp.com',         // <-- ADD
-            'https://the-foothill-herald.herokuapp.com'         // <-- ADD
-        ]
-    }))
+    app.use(cors())
     app.use(express.json())
 
     app.use("/user", userRoutes)
