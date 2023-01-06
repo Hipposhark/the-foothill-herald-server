afd858208ec8142d901682d8b0fbc373d3a57bf7
Hipposhark <edwin.li@outlook.com>
2023-01-06 15:45:37 -0800

    dirname1

--- src/middlewares/serving.middleware.mjs
+++ src/middlewares/serving.middleware.mjs
@@ -1,5 +1,8 @@
+import path from 'path';
+
 export const makeServingMiddleware = ({}) => {
     return (req, res, next) => {
+      const __dirname = path.resolve();
         // If no previous routes match the request, send back the React app.
         res.sendFile(__dirname + "/public/index.html");
       }
