794d374bb251a37a2e9a504e9a966c9df82a8ac1
Hipposhark <edwin.li@outlook.com>
2023-01-06 14:17:11 -0800

    changed .env.local & removed serving middleware

--- src/app.mjs
+++ src/app.mjs
@@ -1,15 +1,13 @@
 import express from "express";
 import cors from "cors";
 
-export const app = ({userRoutes, articleRoutes, servingMiddleware, errorMiddleware}) => {
+export const app = ({userRoutes, articleRoutes, errorMiddleware}) => {
     const app = express()
     app.use(cors())
     app.use(express.json())
 
     app.use("/user", userRoutes)
     app.use("/article", articleRoutes)
-
-    app.use(servingMiddleware)
 
     app.use(errorMiddleware)
     return app
