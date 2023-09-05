ed6591da33f212ff48127cb410fc08cdb4bd77ea
Hipposhark <edwin.li@outlook.com>
2023-09-04 18:48:18 -0700

    added & implemented cors option to prevent No 'Access-Control-Allow-Origin'

--- src/app.mjs
+++ src/app.mjs
@@ -2,8 +2,15 @@ import express from "express";
 import cors from "cors";
 
 export const app = ({userRoutes, articleRoutes, servingMiddleware, errorMiddleware}) => {
+
+    const corsOptions ={
+       origin:'*', 
+       credentials:true,
+       optionSuccessStatus:200,
+    }
+     
     const app = express()
-    app.use(cors())
+    app.use(cors(corsOptions))
     app.use(express.json())
 
     app.use("/user", userRoutes)
