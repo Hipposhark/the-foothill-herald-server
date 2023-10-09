92e64ef2d0de6615f62e7bb6883440997b8b9654
Hipposhark <edwin.li@outlook.com>
2023-10-08 23:53:58 -0700

    removed specific cors stuff

--- src/app.mjs
+++ src/app.mjs
@@ -2,20 +2,9 @@ import express from "express";
 import cors from "cors";
 
 export const app = ({userRoutes, articleRoutes, servingMiddleware, errorMiddleware}) => {
-
-    const corsOptions ={
-       origin: [
-            "https://vercel.live/link/the-foothill-herald-git-new-i-561fb3-thefoothillherald-gmailcom.vercel.app?via=deployment-domains-list-branch",
-            "https://vercel.live/link/the-foothill-herald-3zvlgcisr-thefoothillherald-gmailcom.vercel.app?via=deployment-domains-list-commit",
-            "https://the-foothill-herald-thefoothillherald-gmailcom.vercel.app/",
-            "https://www.thefoothillherald.com",
-        ], 
-       credentials:true,
-       optionSuccessStatus:200,
-    }
      
     const app = express()
-    app.use(cors(corsOptions))
+    app.use(cors())
     app.use(express.json())
 
     app.use("/user", userRoutes)
