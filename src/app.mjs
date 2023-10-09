382649da53d1dfd5cc3f588c45361e74f4819c7f
Hipposhark <edwin.li@outlook.com>
2023-10-09 00:22:05 -0700

    changed cor option to '*'

--- src/app.mjs
+++ src/app.mjs
@@ -2,9 +2,19 @@ import express from "express";
 import cors from "cors";
 
 export const app = ({userRoutes, articleRoutes, servingMiddleware, errorMiddleware}) => {
+
+    const corsOptions ={
+       origin: [
+            "https://vercel.live/link/the-foothill-herald-git-new-i-561fb3-thefoothillherald-gmailcom.vercel.app?via=deployment-domains-list-branch",
+            "https://vercel.live/link/the-foothill-herald-3zvlgcisr-thefoothillherald-gmailcom.vercel.app?via=deployment-domains-list-commit",
+            "https://the-foothill-herald-thefoothillherald-gmailcom.vercel.app/",
+            "https://www.thefoothillherald.com",
+            "http://localhost:3000/",
+        ]
+    }
      
     const app = express()
-    app.use(cors())
+    app.use(cors("*"))
     app.use(express.json())
 
     app.use("/user", userRoutes)
